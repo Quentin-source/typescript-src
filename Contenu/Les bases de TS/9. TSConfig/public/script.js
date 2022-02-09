f31a24e9c864788875652fd808e8f408987358ba
Quentin-source <carlier.quentin@gmail.com>
2022-02-09 10:06:31 +0100

    TSConfig Base

---
+++ Contenu/Les bases de TS/9. TSConfig/public/script.js
@@ -0,0 +1,4 @@
+"use strict";
+// possible avec es6 pas es5
+const pNodeList = document.querySelectorAll('p');
+const arrFromNodeList = [...pNodeList];
---
+++ Contenu/Les bases de TS/9. TSConfig/src/script.ts
@@ -0,0 +1,4 @@
+
+// possible avec es6 pas es5
+const pNodeList = document.querySelectorAll('p');
+const arrFromNodeList = [...pNodeList];
