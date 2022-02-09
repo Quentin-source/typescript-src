3b7afdd8bd287112336ebbd4b45ac1198812cb56
Quentin-source <carlier.quentin@gmail.com>
2022-02-09 15:49:21 +0100

    add Remove item

--- projects/trello-clone/public/app.js
+++ projects/trello-clone/public/app.js
@@ -5,8 +5,12 @@ const itemsContainer = document.querySelectorAll(".items-container");
 function addContainerListeners(currentContainer) {
     const currentContainerDeletionBtn = currentContainer.querySelector(".delete-container-btn");
     const currentAddItemBtn = currentContainer.querySelector(".add-item-btn");
+    const currentCloseFormBtn = currentContainer.querySelector(".close-form-btn");
+    const currentForm = currentContainer.querySelector("form");
     deleteBtnListeners(currentContainerDeletionBtn);
     addItemsBtnListeners(currentAddItemBtn);
+    closingFormsBtnListeners(currentCloseFormBtn);
+    addFormSubmitListeners(currentForm);
 }
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
@@ -16,6 +20,12 @@ function deleteBtnListeners(btn) {
 }
 function addItemsBtnListeners(btn) {
     btn.addEventListener("click", handleAddItem);
+}
+function closingFormsBtnListeners(btn) {
+    btn.addEventListener("click", () => toggleForm(actualBtn, actualForm, false));
+}
+function addFormSubmitListeners(form) {
+    form.addEventListener("submit", createNewItem);
 }
 function handleContainerDeletion(event) {
     const btn = event.target;
@@ -51,4 +61,30 @@ function toggleForm(btn, form, action) {
         form.style.display = "block";
         btn.style.display = "none";
     }
+}
+function createNewItem(event) {
+    event.preventDefault();
+    if (actualTextInput.value.length === 0) {
+        actualValidation.textContent = "Must be at least 1 character long";
+        return;
+    }
+    else {
+        actualValidation.textContent = "";
+    }
+    const itemContent = actualTextInput.value;
+    const li = `<li class="item" draggable="true">
+    <p>${itemContent}</p>
+    <button>X</button>
+    </li>`;
+    actualUL.insertAdjacentHTML("beforeend", li);
+    const item = actualUL.lastElementChild;
+    const liBtn = item.querySelector("button");
+    handleItemDeletion(liBtn);
+    actualTextInput.value = "";
+}
+function handleItemDeletion(btn) {
+    btn.addEventListener("click", () => {
+        const elToRemove = btn.parentElement;
+        elToRemove.remove();
+    });
 }
--- projects/trello-clone/src/app.ts
+++ projects/trello-clone/src/app.ts
@@ -18,9 +18,15 @@ function addContainerListeners(currentContainer: HTMLDivElement) {
   const currentAddItemBtn = currentContainer.querySelector(
     ".add-item-btn"
   ) as HTMLButtonElement;
+  const currentCloseFormBtn = currentContainer.querySelector(
+    ".close-form-btn"
+  ) as HTMLButtonElement;
+  const currentForm = currentContainer.querySelector("form") as HTMLFormElement;
 
   deleteBtnListeners(currentContainerDeletionBtn);
   addItemsBtnListeners(currentAddItemBtn);
+  closingFormsBtnListeners(currentCloseFormBtn);
+  addFormSubmitListeners(currentForm);
 }
 
 itemsContainer.forEach((container: HTMLDivElement) => {
@@ -33,6 +39,14 @@ function deleteBtnListeners(btn: HTMLButtonElement) {
 
 function addItemsBtnListeners(btn: HTMLButtonElement) {
   btn.addEventListener("click", handleAddItem);
+}
+
+function closingFormsBtnListeners(btn: HTMLButtonElement) {
+  btn.addEventListener("click", () => toggleForm(actualBtn, actualForm, false));
+}
+
+function addFormSubmitListeners(form: HTMLFormElement) {
+  form.addEventListener("submit", createNewItem);
 }
 
 function handleContainerDeletion(event: MouseEvent) {
@@ -76,4 +90,31 @@ function toggleForm(
     form.style.display = "block";
     btn.style.display = "none";
   }
+}
+
+function createNewItem(event: Event) {
+  event.preventDefault();
+  if (actualTextInput.value.length === 0) {
+    actualValidation.textContent = "Must be at least 1 character long";
+    return;
+  } else {
+    actualValidation.textContent = "";
+  }
+  const itemContent = actualTextInput.value;
+  const li = `<li class="item" draggable="true">
+    <p>${itemContent}</p>
+    <button>X</button>
+    </li>`;
+  actualUL.insertAdjacentHTML("beforeend", li);
+  const item = actualUL.lastElementChild as HTMLLIElement;
+  const liBtn = item.querySelector("button") as HTMLButtonElement;
+  handleItemDeletion(liBtn);
+  actualTextInput.value = "";
+}
+
+function handleItemDeletion(btn: HTMLButtonElement) {
+  btn.addEventListener("click", () => {
+    const elToRemove = btn.parentElement as HTMLLIElement;
+    elToRemove.remove();
+  });
 }
